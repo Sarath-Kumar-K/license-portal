4da3aa3aac65c3dc5aaed2528f0def60cd7fa4de
sarathkumar.krishnamoorthi <sarathkumar.krishnamoorthi@vembu.com>
2026-02-23 23:38:56 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/ksk/portal/LicensePortalApplication.java
@@ -0,0 +1,13 @@
+package com.ksk.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicensePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicensePortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ksk/portal/LicensePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksk.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicensePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
